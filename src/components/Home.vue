b29df1221df2804fc5a2db19c33936009b12ba02
adekang <201497709@qq.com>
2021-05-29 16:36:56 +0800

    侧边栏 插入图标实现动画效果

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,13 +9,19 @@
         <el-button type="info" @click="logout">退出</el-button>
       </el-header>
       <el-container>
-        <el-aside width="200px">
+        <el-aside :width="isCollapse? '64px': '200px'">
+          <!-- 菜单折叠 -->
+          <div class="toggle-button" @click="toggleCollapse">
+            |||
+          </div>
           <!-- 侧边栏菜单 -->
           <el-menu
               background-color="#333744"
               text-color="#fff"
               active-text-color="#409eff"
-              unique-opened>
+              unique-opened
+              :collapse="isCollapse"
+              :collapse-transition="false">
             <!-- 一级菜单 -->
             <el-submenu :index="item.id+''" v-for="item in menuList" :key="item.id">
               <!-- 一级菜单模板 -->
@@ -53,7 +59,8 @@ export default {
         '101': 'iconfont icon-shangpin',
         '102': 'iconfont icon-danju',
         '145': 'iconfont icon-baobiao'
-      }
+      },
+      isCollapse: false
     }
   },
   created() {
@@ -69,6 +76,10 @@ export default {
       const {data: res} = await this.$http.get('menus')
       if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
       this.menuList = res.data
+    },
+    //点击按钮切换菜单的折叠和展开
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse
     }
   }
 
@@ -106,6 +117,17 @@ export default {
 
 .el-aside {
   background-color: #333744;
+  transition: all 250ms linear;
+
+  > .toggle-button {
+    background-color: #4A5064;
+    font-size: 10px;
+    line-height: 24px;
+    color: white;
+    text-align: center;
+    letter-spacing: 0.2em;
+    cursor: pointer;
+  }
 
   .el-menu {
     border-right: none;
