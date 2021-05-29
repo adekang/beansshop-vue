17befe6e9016e443bd5c086ef42a0b4ad1307374
adekang <201497709@qq.com>
2021-05-29 10:34:30 +0800

    创建路由控制守卫

--- src/router/index.js
+++ src/router/index.js
@@ -15,4 +15,16 @@ const router = new VueRouter({
   routes,
 })
 
+// 挂载路由守卫
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next()
+
+//   获取Token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) {
+    return next('./login')
+  }
+  next()
+
+})
 export default router
