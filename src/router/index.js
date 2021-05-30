5be1d4ba1fe0bd01774230228b884ccaca8ce41d
adekang <201497709@qq.com>
2021-05-30 10:39:27 +0800

    实现用户权限列表的面包屑导航 和数据展示

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
 import NotFound from '../components/NotFound.vue'
 import Users from '@/components/user/users.vue'
+import Rights from '@/components/power/Rights.vue'
 
 Vue.use(VueRouter)
 
@@ -18,6 +19,7 @@ const routes = [
     children: [
       {path: '/welcome', component: Welcome},
       {path: '/users', component: Users},
+      {path: '/rights', component: Rights},
     ]
   },
   // 404页面
