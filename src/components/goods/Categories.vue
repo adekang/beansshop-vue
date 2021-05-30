099e07f048c6196fd96e59304054b73c7599835f
adekang <201497709@qq.com>
2021-05-30 19:58:55 +0800

    商品分类 基础布局OK

--- src/components/goods/Categories.vue
+++ src/components/goods/Categories.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
-    商品分类
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!--卡片视图区域-->
+    <el-card>
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加分类</el-button>
+        </el-col>
+      </el-row>
+
+
+      <!--表格区域  -->
+
+      <!--分页区域-->
+    </el-card>
   </div>
 </template>
 
