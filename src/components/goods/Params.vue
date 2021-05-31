05a646d1e1f19c81f42abd177f88176e0c7cf12a
adekang <201497709@qq.com>
2021-05-31 10:35:36 +0800

    分类参数 创建路由

---
+++ src/components/goods/Params.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Params.vue
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Users from '@/components/user/users.vue'
 import Rights from '@/components/power/Rights.vue'
 import Roles from '@/components/power/Roles.vue'
 import Categories from '@/components/goods/Categories.vue'
+import Params from '@/components/goods/Params.vue'
 
 Vue.use(VueRouter)
 
@@ -24,6 +25,7 @@ const routes = [
       {path: '/rights', component: Rights},
       {path: '/roles', component: Roles},
       {path: '/categories', component: Categories},
+      {path: '/params', component: Params},
     ]
   },
   // 404页面
