bbf7b4c78999d02ad4ed562fac675b62f6dae1dc
adekang <201497709@qq.com>
2021-05-31 15:17:34 +0800

    分类参数 级联菜单数据展示

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -22,6 +22,46 @@
         </el-col>
         <el-col></el-col>
       </el-row>
+
+      <!-- tab页签区域   -->
+      <el-tabs v-model="activeName" @tab-click="handleTabClick">
+        <!-- 添加动态参数的面板 将标签页改为many -->
+        <el-tab-pane label="动态参数" name="many">
+          <el-button size="mini" type="primary" :disabled="isBtnDisabled">添加参数</el-button>
+          <!-- 动态参数表格 -->
+          <el-table :data="manyTableData" border stripe>
+            <!-- 展开行 -->
+            <el-table-column type="expand"></el-table-column>
+            <!-- 索引列 -->
+            <el-table-column type="index"></el-table-column>
+            <el-table-column label="参数名称" prop="attr_name"></el-table-column>
+            <el-table-column label="操作">
+              <template>
+                <el-button size="mini" type="primary" icon="el-icon-edit">编辑</el-button>
+                <el-button size="mini" type="danger" icon="el-icon-delete">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+        <!-- 添加静态属性的面板 将标签页改为only -->
+        <el-tab-pane label="静态属性" name="only">
+          <el-button size="mini" type="primary" :disabled="isBtnDisabled">添加属性</el-button>
+          <!-- 静态属性表格 -->
+          <el-table :data="onlyTableData" border stripe>
+            <!-- 展开行 -->
+            <el-table-column type="expand"></el-table-column>
+            <!-- 索引列 -->
+            <el-table-column type="index"></el-table-column>
+            <el-table-column label="属性名称" prop="attr_name"></el-table-column>
+            <el-table-column label="操作">
+              <template>
+                <el-button size="mini" type="primary" icon="el-icon-edit">编辑</el-button>
+                <el-button size="mini" type="danger" icon="el-icon-delete">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+      </el-tabs>
     </el-card>
   </div>
 </template>
@@ -37,7 +77,13 @@ export default {
         label: 'cat_name',
         children: 'children'
       },
-      selectedCateKeys: []
+      selectedCateKeys: [],
+      //tab页签激活显示的页签项
+      activeName: 'many',
+      //用来保存动态参数数据
+      manyTableData: [],
+      //用来保存静态属性数据
+      onlyTableData: []
     }
   },
   created() {
@@ -50,11 +96,45 @@ export default {
         return this.$message.error('获取商品分类失败！')
       }
       this.cateList = res.data
-      console.log(this.cateList)
     },
-    handleChange(){
+    handleChange() {
+      this.getParamsData()
+    },
+    async getParamsData() {
       //当用户在级联菜单中选择内容改变时触发
-      console.log(this.selectedCateKeys)
+      if (this.selectedCateKeys.length !== 3) {
+        this.selectedCateKeys = []
+        return this.$message.error('只允许为第三级分类设置相关参数')
+      }
+      //发送请求，根据用户选择的三级分类和面板获取参数数据
+      const {data: res} = await this.$http.get(
+          `categories/${this.cateId}/attributes`,
+          {params: {sel: this.activeName}}
+      )
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取参数列表数据失败')
+      }
+      if (this.activeName === 'many') {
+        //获取的是动态参数
+        this.manyTableData = res.data
+      } else if (this.activeName === 'only') {
+        //获取的是静态属性
+        this.onlyTableData = res.data
+      }
+    },
+    handleTabClick() {
+      this.getParamsData()
+    }
+  },
+  computed: {
+    isBtnDisabled() {
+      return this.selectedCateKeys.length !== 3
+    },
+    cateId() {
+      if (this.selectedCateKeys.length === 3) {
+        return this.selectedCateKeys[2]
+      }
+      return null
     }
   }
 }
