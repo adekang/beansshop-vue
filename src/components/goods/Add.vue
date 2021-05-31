e69aeb56af99fa5f1dd53f67ac93f55864f1635a
adekang <201497709@qq.com>
2021-05-31 20:25:23 +0800

    商品列表 添加商品页面第一步骤布局和功能

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -1,13 +1,129 @@
 <template>
   <div>
+    <h3>添加商品</h3>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>添加商品</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <!-- 消息提示 -->
+      <el-alert title="添加商品信息" type="info" center show-icon :closable="false">
+      </el-alert>
+      <!-- 步骤条组件 -->
+      <el-steps :space="200" :active="activeIndex - 0" finish-status="success" align-center>
+        <el-step title="基本信息"></el-step>
+        <el-step title="商品参数"></el-step>
+        <el-step title="商品属性"></el-step>
+        <el-step title="商品图片"></el-step>
+        <el-step title="商品内容"></el-step>
+        <el-step title="完成"></el-step>
+      </el-steps>
+      <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="100px" label-position="top">
+        <el-tabs v-model="activeIndex" :tab-position="'left'">
+          <el-tab-pane label="基本信息" name="0">
+            <el-form-item label="商品名称" prop="goods_name">
+              <el-input v-model="addForm.goods_name"></el-input>
+            </el-form-item>
+            <el-form-item label="商品价格" prop="goods_price">
+              <el-input v-model="addForm.goods_price" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品重量" prop="goods_weight">
+              <el-input v-model="addForm.goods_weight" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品数量" prop="goods_number">
+              <el-input v-model="addForm.goods_number" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品分类" prop="goods_cat">
+              <!-- 选择商品分类的级联选择框 -->
+              <el-cascader expandTrigger='hover' v-model="addForm.goods_cat" :options="cateList" :props="cateProps"
+                           @change="handleChange" clearable></el-cascader>
+            </el-form-item>
+          </el-tab-pane>
+          <el-tab-pane label="商品参数" name="1">
 
+          </el-tab-pane>
+          <el-tab-pane label="商品属性" name="2">
+
+          </el-tab-pane>
+          <el-tab-pane label="商品图片" name="3">
+
+          </el-tab-pane>
+          <el-tab-pane label="商品内容" name="4">
+
+          </el-tab-pane>
+        </el-tabs>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      //保存步骤条激活项索引
+      activeIndex: '0',
+      //添加商品的表单数据对象
+      addForm: {
+        goods_name: '',
+        goods_price: 0,
+        goods_weight: 0,
+        goods_number: 0,
+        goods_cat: []
+      },
+      //验证规则
+      addFormRules: {
+        goods_name: [
+          {required: true, message: '请输入商品名称', trigger: 'blur'}
+        ],
+        goods_price: [
+          {required: true, message: '请输入商品价格', trigger: 'blur'}
+        ],
+        goods_weight: [
+          {required: true, message: '请输入商品重量', trigger: 'blur'}
+        ],
+        goods_number: [
+          {required: true, message: '请输入商品数量', trigger: 'blur'}
+        ],
+        goods_cat: [
+          {required: true, message: '请选择商品分类', trigger: 'blur'}
+        ]
+      },
+      //用来保存分类数据
+      cateList: [],
+      //配置级联菜单中数据如何展示
+      cateProps: {
+        value: 'cat_id',
+        label: 'cat_name',
+        children: 'children'
+      }
+    }
+  },
+  created() {
+    this.getCateList()
+  },
+  methods: {
+    async getCateList() {
+      const {data: res} = await this.$http.get('categories')
+
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品分类数据失败')
+      }
+      this.cateList = res.data
+    },
+    handleChange() {
+      //如果用户选择的不是三级分类,该次选择无效，因为必须选择三级分类
+      if (this.addForm.goods_cat.length !== 3) {
+        this.$message.error('选择的不是三级分类,该次选择无效!')
+        this.addForm.goods_cat = []
+      }
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
