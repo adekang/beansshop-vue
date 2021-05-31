333437596684be8213fe0ec931e4732ac89eb5c0
adekang <201497709@qq.com>
2021-05-31 20:02:36 +0800

    商品列表 实现页面分页

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -9,6 +9,7 @@
     </el-breadcrumb>
     <!-- 卡片视图区域 -->
     <el-card>
+      <!-- 搜索栏 -->
       <el-row :gutter="20">
         <el-col :span="8">
           <el-input placeholder="请输入内容">
@@ -19,14 +20,18 @@
           <el-button type="primary">添加商品</el-button>
         </el-col>
       </el-row>
-
+      <!-- 表格区域 -->
       <el-table :data="goodsList" border stripe>
         <el-table-column type="index"></el-table-column>
         <el-table-column label="商品名称" prop="goods_name"></el-table-column>
-        <el-table-column label="商品价格（元）" prop="goods_price"></el-table-column>
-        <el-table-column label="商品重量" prop="goods_weight"></el-table-column>
-        <el-table-column label="创建时间" prop="add_time"></el-table-column>
-        <el-table-column label="操作">
+        <el-table-column label="商品价格（元）" prop="goods_price" width="90px"></el-table-column>
+        <el-table-column label="商品重量" prop="goods_weight" width="90px"></el-table-column>
+        <el-table-column label="创建时间" width="140px">
+          <template slot-scope="scope">
+            {{ scope.row.add_time | dateFormat }}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="200px">
           <el-button size="mini" type="primary" icon="el-icon-edit">
             编辑
           </el-button>
@@ -34,6 +39,11 @@
           </el-button>
         </el-table-column>
       </el-table>
+      <!-- 分页 -->
+      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
+                     :current-page="queryInfo.pagenum" :page-sizes="[3, 5, 10, 15]" :page-size="queryInfo.pagesize"
+                     layout="total, sizes, prev, pager, next, jumper" :total="total">
+      </el-pagination>
     </el-card>
   </div>
 </template>
@@ -69,7 +79,15 @@ export default {
         return this.$message.error('获取商品列表失败')
       }
       this.goodsList = res.data.goods
-      this.total - res.data.total
+      this.total = res.data.total
+    },
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getGoodsList()
+    },
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage
+      this.getGoodsList()
     }
   }
 }
@@ -78,3 +96,4 @@ export default {
 <style lang="scss" scoped>
 
 </style>
+
