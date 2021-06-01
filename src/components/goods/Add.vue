abbb20776743daba72306180ad21d8844d58e286
adekang <201497709@qq.com>
2021-06-01 22:26:26 +0800

    打包开发运用不同main.js

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -156,11 +156,9 @@ export default {
       }
 
       this.catelist = res.data
-      console.log(this.catelist)
     },
     // 级联选择器选中项变化，会触发这个函数
     handleChange() {
-      console.log(this.addForm.goods_cat)
       if (this.addForm.goods_cat.length !== 3) {
         this.addForm.goods_cat = []
       }
@@ -175,7 +173,6 @@ export default {
       }
     },
     async tabClicked() {
-      // console.log(this.activeIndex)
       // 证明访问的是动态参数面板
       if (this.activeIndex === '1') {
         const {data: res} = await this.$http.get(
@@ -189,7 +186,6 @@ export default {
           return this.$message.error('获取动态参数列表失败！')
         }
 
-        console.log(res.data)
         res.data.forEach(item => {
           item.attr_vals = item.attr_vals.length === 0 ? [] : item.attr_vals.split(' ')
         })
@@ -206,35 +202,29 @@ export default {
           return this.$message.error('获取静态属性失败！')
         }
 
-        console.log(res.data)
         this.onlyTableData = res.data
       }
     },
     // 处理图片预览效果
     handlePreview(file) {
-      console.log(file)
       this.previewPath = file.response.data.url
       this.previewVisible = true
     },
     // 处理移除图片的操作
     handleRemove(file) {
-      // console.log(file)
       // 1. 获取将要删除的图片的临时路径
       const filePath = file.response.data.tmp_path
       // 2. 从 pics 数组中，找到这个图片对应的索引值
       const i = this.addForm.pics.findIndex(x => x.pic === filePath)
       // 3. 调用数组的 splice 方法，把图片信息对象，从 pics 数组中移除
       this.addForm.pics.splice(i, 1)
-      console.log(this.addForm)
     },
     // 监听图片上传成功的事件
     handleSuccess(response) {
-      console.log(response)
       // 1. 拼接得到一个图片信息对象
       const picInfo = {pic: response.data.tmp_path}
       // 2. 将图片信息对象，push 到pics数组中
       this.addForm.pics.push(picInfo)
-      console.log(this.addForm)
     },
     // 添加商品
     add() {
@@ -260,7 +250,6 @@ export default {
           this.addForm.attrs.push(newInfo)
         })
         form.attrs = this.addForm.attrs
-        console.log(form)
 
         // 发起请求添加商品
         // 商品的名称，必须是唯一的
