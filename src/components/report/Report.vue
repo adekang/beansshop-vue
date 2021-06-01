b1da061d82b39b6227589947f60c7c603a232be9
adekang <201497709@qq.com>
2021-06-01 20:50:14 +0800

    数据报表 新建路由以及面包屑导航

---
+++ src/components/report/Report.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card></el-card>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
