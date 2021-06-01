1305386e1469b6f86621e6d505b9c35ad3d5f625
adekang <201497709@qq.com>
2021-06-01 20:11:51 +0800

    订单列表 搜索框，面包屑导航

---
+++ src/components/order/Order.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+      <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <!-- 搜索栏 -->
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input placeholder="请输入内容" v-model="queryInfo.query" clearable>
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      //查询条件
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10
+      }
+    }
+  },
+  created() {
+
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
